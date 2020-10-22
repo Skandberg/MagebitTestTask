8394236070fa0041ef93272065399efd4b7f912c
Skandberg <ilarion.larin@gmail.com>
2020-10-22 20:46:24 +0300

    Fourth commit

--- html/validation.js
+++ html/validation.js
@@ -1,4 +1,60 @@
-function validate_email(termsCheckBox){
+$(document).ready(function(){
+    $.fn.validate_email=function(){
+        let form=document.getElementById("form");
+        let email=document.getElementById("email").value;
+        let errors=document.getElementById("errors");
+        let termsCheckBox=document.getElementById("terms"); 
+                        
+
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+        let invalid_email="Please enter a valid email ";
+        let columbia="We don't accept emails from Columbia ";
+        let empty="Email box cannot be empty ";
+        let not_checked="You must be agree with our terms of use "
+
+        if (email.match(pattern) && termsCheckBox.checked){
+            form.classList.remove("invalid");
+            errors.innerHTML="";
+            document.getElementById("subscribe").disabled = false;
+                        
+        }else if(!termsCheckBox.checked){
+            form.classList.add("invalid");
+            errors.innerHTML=not_checked
+            document.getElementById("subscribe").disabled = true;
+        }else if(!email.match(pattern)){
+            form.classList.add("invalid");
+            errors.innerHTML=invalid_email;
+            document.getElementById("subscribe").disabled = true;
+        } 
+        if(email==""){
+            form.classList.add("invalid");
+            errors.innerHTML=empty;
+            document.getElementById("subscribe").disabled = true;
+        }
+        if (email.slice(-3)=='.co'){
+            form.classList.add("invalid");
+            errors.innerHTML=columbia;
+            document.getElementById("subscribe").disabled = true;
+        }
+    }
+    $("#terms").click(function(){
+        $.fn.validate_email();
+    });
+    $("#email").keydown(function(){
+        $.fn.validate_email();
+    });
+    $("#subscribe").click(function(){
+        $("#form").remove();
+        $(".subscription").append('<p class=`description`>Successfully subscribed!</p>')
+    });
+    
+});
+
+
+
+
+/*function validate_email(termsCheckBox){
     let form=document.getElementById("form");
     let email=document.getElementById("email").value;
     let errors=document.getElementById("errors"); 
@@ -36,3 +92,4 @@ function validate_email(termsCheckBox){
                 document.getElementById("subscribe").disabled = true;
             }
 }
+*/
